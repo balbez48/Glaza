e36c56c3913821e6cf7feb3fd5b4bb69caeff2bb
balbez <mainmail218@gmail.com>
2026-04-02 15:21:58 +0300

    дописывал камеру

--- Frontend/Glaza/src/app/index.tsx
+++ Frontend/Glaza/src/app/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, Text, Button, TouchableOpacity } from 'react-native';
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import { useRef } from 'react';
+import { describeImage } from '../services/gigachat'
 
 export default function MainWindow() {
     const cameraRef = useRef<CameraView>(null);
@@ -24,9 +25,29 @@ export default function MainWindow() {
         );
     }
 
+    async function sendPicture() {
+        if (cameraRef.current) {
+            const photo = await cameraRef.current.takePictureAsync({
+                quality: 0.8,
+                base64: true,
+            });
+
+            if (photo.base64) {
+                describeImage(photo.base64);
+            } else {
+                console.error('Не получили base64');
+            }
+        }
+    }
+
     return (
         <View style={styles.container}>
             <CameraView ref={cameraRef} style={styles.camera} />
+            <View style={styles.buttonCont}>
+                <TouchableOpacity style={styles.button} onPress={sendPicture}>
+                    <Text style={styles.textBtn}>Сделать снимок</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 }
@@ -34,7 +55,7 @@ export default function MainWindow() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'green',
+        backgroundColor: 'blue',
         justifyContent: 'center',
         alignItems: 'center',
     },
@@ -42,4 +63,8 @@ const styles = StyleSheet.create({
         flex: 1,
         width: '100%',
     },
+    buttonCont: {},
+    button: {},
+    textBtn: {},
+
 });
--- Frontend/Glaza/src/services/gigachat.ts
+++ Frontend/Glaza/src/services/gigachat.ts
@@ -40,14 +40,8 @@ async function getAccessToken() {
     }
 }
 
-async function describeImage(cameraRef: RefObject<CameraView>) {
+export async function describeImage(base64: String) {
     const token = await getAccessToken();
-    const photo = await cameraRef.current.takePictureAsync({
-        quality: 0.8,
-        base64: true,
-    });
-
-    const base64 = photo.base64;
     const url = 'https://gigachat.devices.sberbank.ru/api/v1/chat/completions';
 
     const headers = {
